61553d4289892cb257492dff88d15f53964fbd4b
Ibad-ur-Rehman <ibadkps@gmail.com>
2023-12-13 07:26:59 +0500

    merge sort implemented

--- function.py
+++ function.py
@@ -23,13 +23,47 @@ def radix_sort(my_list, max_len):
         n *= 10
         print(my_list)
 
+def merge_sort(my_list):
+    # case where list have only one or zero 0 node then list is already sorted
+    if len(my_list) <= 1:
+        return my_list
+    # finding length of list
+    mid = find_mid(my_list)
+    left_list = my_list[mid:]
+    right_list = my_list[:mid]
+    left = merge_sort(left_list)
+    right = merge_sort(right_list)
+    return merge_list(left, right)
 
+def find_mid(listt):
+    total_length = len(listt)
+    mid = int(total_length // 2)
+    return mid
 
+def merge_list(left, right):
+    result = []
+    i = j = 0
 
+    # Comparing elements from left and right sublists and merging them
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
 
+    # Add the remaining elements from left and right
+    result.extend(left[i:])
+    result.extend(right[j:])
 
-
+    return result
 
 if __name__ == "__main__":
-    list_my = [23, 1, 45, 234, 5, 7, 89]
-    radix_sort(list_my, max_len=3)
+    # list_my = [23, 1, 45, 234, 5, 7, 89]
+    # radix_sort(list_my, max_len=3)
+
+    my_list = [38, 27, 43, 3, 9, 82, 10]
+    sorted_list = merge_sort(my_list)
+    print("Original List:", my_list)
+    print("Sorted List:", sorted_list)
